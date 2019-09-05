8dabf1d3776016b864356ff2ea3e3d4855952496
Vegctrp <kem.altair@gmail.com>
2019-09-05 19:08:22 +0900

    solve 054~057

---
+++ q51_60/055.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import lib5160
+import matplotlib.pyplot as plt
+import sys,os
+sys.path.append(os.getcwd())
+from q01_10 import lib0110
+from q41_50 import lib4150
+
+if __name__ == '__main__':
+    img = cv2.imread("Gasyori100knock/Question_51_60/imori.jpg")
+    part = cv2.imread("Gasyori100knock/Question_51_60/imori_part.jpg")
+    
+    out = lib5160.Matching_SAD(img, part)
+
+    cv2.imshow("imori", out)
+    cv2.waitKey(0)
+    cv2.imwrite("q51_60/055.jpg", out)
+    cv2.destroyAllWindows()
--- q51_60/lib5160.py
+++ q51_60/lib5160.py
@@ -18,7 +18,6 @@ def Morphology_gradient(img): # for binarizated-image
 # 52
 def Tophat_transform(img): # for binarizated-image
     ope = lib4150.Opening_operation(img, 3)
-    #ope = lib4150.Closing_operation(img, 3)
     return (img - ope).clip(0,255)
 
 
@@ -26,3 +25,87 @@ def Tophat_transform(img): # for binarizated-image
 def Blackhat_transform(img): # for binarizated-image
     ope = lib4150.Closing_operation(img, 3)
     return (ope - img).clip(0,255)
+
+
+# 54
+def Matching_SSD(img, part):
+    i2 = img.astype(np.float64)
+    p2 = part.astype(np.float64)
+    iH, iW, iC = img.shape
+    pH, pW, pC = part.shape
+    min_x = -1
+    min_y = -1
+    min_value = (255**2)*iH*iW*iC
+    for y in range(iH-pH):
+        for x in range(iW-pW):
+            v = np.sum((i2[y:y+pH, x:x+pW, :] - p2) ** 2)
+            if min_value > v:
+                min_x = x
+                min_y = y
+                min_value = v
+    out = cv2.rectangle(img, (min_x, min_y), (min_x+pW-1, min_y+pH-1), (0,0,255), thickness=1)
+    return out
+
+
+# 55
+def Matching_SAD(img, part):
+    i2 = img.astype(np.float64)
+    p2 = part.astype(np.float64)
+    iH, iW, iC = img.shape
+    pH, pW, pC = part.shape
+    min_x = -1
+    min_y = -1
+    min_value = (255**2)*iH*iW*iC
+    for y in range(iH-pH):
+        for x in range(iW-pW):
+            v = np.sum(np.abs(i2[y:y+pH, x:x+pW, :] - p2))
+            if min_value > v:
+                min_x = x
+                min_y = y
+                min_value = v
+    out = cv2.rectangle(img, (min_x, min_y), (min_x+pW-1, min_y+pH-1), (0,0,255), thickness=1)
+    return out
+
+
+# 56
+def Matching_NCC(img, part):
+    i2 = img.astype(np.float64)
+    p2 = part.astype(np.float64)
+    iH, iW, iC = img.shape
+    pH, pW, pC = part.shape
+    max_x = -1
+    max_y = -1
+    max_value = -2.0
+    for y in range(iH-pH):
+        for x in range(iW-pW):
+            S = np.sum(i2[y:y+pH, x:x+pW, :] * p2) / (np.sqrt(np.sum(i2[y:y+pH, x:x+pW, :]**2) * np.sqrt(np.sum(p2**2))))
+            if max_value < S:
+                max_x = x
+                max_y = y
+                max_value = S
+    out = cv2.rectangle(img, (max_x, max_y), (max_x+pW-1, max_y+pH-1), (0,0,255), thickness=1)
+    return out
+
+
+# 57
+def Matching_ZNCC(img, part):
+    i2 = img.astype(np.float64)
+    p2 = part.astype(np.float64)
+    pmean = np.mean(i2, axis=(0, 1))
+    p2 = p2 - pmean
+    iH, iW, _ = img.shape
+    pH, pW, _ = part.shape
+    max_x = -1
+    max_y = -1
+    max_value = -2.0
+    for y in range(iH-pH):
+        for x in range(iW-pW):
+            imean = np.mean(i2[y:y+pH, x:x+pW, :], axis=(0, 1))
+            i3 = i2 - imean
+            S = np.sum(i3[y:y+pH, x:x+pW, :] * p2) / (np.sqrt(np.sum(i3[y:y+pH, x:x+pW, :]**2)) * np.sqrt(np.sum(p2**2)))
+            if max_value < S:
+                max_x = x
+                max_y = y
+                max_value = S
+    out = cv2.rectangle(img, (max_x, max_y), (max_x+pW-1, max_y+pH-1), (0,0,255), thickness=1)
+    return out
