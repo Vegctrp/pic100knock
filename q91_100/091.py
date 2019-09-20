1fdaea13370899e828cefe4d6ab0aac9f54a2695
Vegctrp <kem.altair@gmail.com>
2019-09-20 15:11:05 +0900

    fix something

---
+++ q91_100/091.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import lib91100
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    img = cv2.imread("Gasyori100knock/Question_91_100/thorino.jpg")
---
+++ q91_100/lib91100.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+from collections import Counter
+import sys,os
+sys.path.append(os.getcwd())
+from q01_10 import lib0110
